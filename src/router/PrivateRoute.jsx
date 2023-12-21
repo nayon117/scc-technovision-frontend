ded8907e69be5a861cf96e404b3d7866fa55d39d
nayon117 <hasibulhasannayon260@gmail.com>
2023-12-21 22:05:08 +0600

    create private route page

--- src/router/PrivateRoute.jsx
+++ src/router/PrivateRoute.jsx
@@ -1,10 +1,26 @@
+import PropTypes from "prop-types";
+import { Navigate, useLocation } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+
+const PrivateRoute = ({ children }) => {
+    const { user,loading } =  useAuth()
+    const location = useLocation()
 
 
-const PrivateRoute = () => {
-  return (
-    <div>
-      <h2>This is PrivateRoute</h2>
-    </div>
-  );
+    if (loading) {
+        return <p className="text-center mt-24"><span className="loading loading-infinity  loading-lg"></span></p>
+  }
+
+  if (user) {
+    return children;
+    }
+  else {
+      return <Navigate state={location.pathname} to='/login'></Navigate>
+    }
 };
+
+PrivateRoute.propTypes = {
+  children: PropTypes.node,
+};
+
 export default PrivateRoute;
